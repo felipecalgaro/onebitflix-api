f326338ea7653619467d93c2589669b63f5820b3
Felipe <pipecalgaro@gmail.com>
2023-01-20 20:55:42 -0300

    Create get all categories route

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,22 @@
-import express from 'express';
-import { adminJS, adminJSRouter } from './adminjs';
-import { sequelize } from './database';
+import express from "express";
+import { adminJS, adminJSRouter } from "./adminjs";
+import { sequelize } from "./database";
+import { router } from "./routes";
 
-const app = express()
+const app = express();
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.use(adminJS.options.rootPath, adminJSRouter)
+app.use(adminJS.options.rootPath, adminJSRouter);
 
-const PORT = process.env.PORT || 3000
+app.use(router);
+
+const PORT = process.env.PORT || 3000;
 
 sequelize.authenticate().then(() => {
-  console.log('connected to database')
+  console.log("connected to database");
 
   app.listen(PORT, () => {
-    console.log('server running on port', PORT);
-  })
-})
+    console.log("server running on port", PORT);
+  });
+});
